f115fd55ee9b208a9e62c1fabfec4aee24ba629e
kojurijan <rijan.koju@mandalabs.com>
2024-04-29 23:01:14 +0545

    update #86ep873ju

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
 
-      <h1>Yo Yo test </h1>
+      <h1>Yo Yo test sroty</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
